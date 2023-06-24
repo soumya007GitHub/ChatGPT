650bb74a14316ee7c586b848b1f60acc738516c3
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2023-06-24 12:37:01 +0530

    Added initial files

---
+++ script.js
@@ -0,0 +1,38 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('chatForm');
+    const userMessageInput = document.getElementById('userMessage');
+    const chatOutputDiv = document.getElementById('chatOutput');
+  
+    form.addEventListener('submit', async (e) => {
+      e.preventDefault(); // Prevent form submission
+  
+      const userMessage = userMessageInput.value;
+  
+      try {
+        const response = await axios.post(
+          'https://api.openai.com/v1/chat/completions',
+          {
+            model : 'gpt-3.5-turbo',
+            messages: [
+              { role: 'system', content: 'You are a helpful assistant.' },
+              { role: 'user', content: userMessage },
+            ],
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer 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', // Replace with your actual API key
+            },
+          }
+        );
+  
+        const reply = response.data.choices[0].message.content;
+        chatOutputDiv.innerHTML = reply;
+      } catch (error) {
+        console.error(error);
+        chatOutputDiv.innerHTML = 'An error occurred';
+      }
+    });
+  });
+  
